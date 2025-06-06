b9440984eaa846289d53285f67f1573cfc59f698
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-06-06 20:20:32 +0100

    minor: add api client configuration

---
+++ src/lib/api.ts
@@ -0,0 +1,26 @@
+import {
+	BaseAPI,
+	CategoriesApi,
+	Configuration,
+	MetadataApi,
+	PostsApi,
+	PreviewsApi,
+	RelatedApi,
+	SearchApi,
+	TagsApi
+} from '../api-client';
+
+const basePath = import.meta.env.VITE_API_BASE_PATH || 'http://localhost:8080/api';
+
+const configuration = new Configuration({
+	basePath: basePath
+});
+
+export const api = new BaseAPI(configuration);
+export const categoriesApi = new CategoriesApi(configuration);
+export const metadataApi = new MetadataApi(configuration);
+export const postsApi = new PostsApi(configuration);
+export const previewsApi = new PreviewsApi(configuration);
+export const relatedApi = new RelatedApi(configuration);
+export const searchApi = new SearchApi(configuration);
+export const tagsApi = new TagsApi(configuration);
