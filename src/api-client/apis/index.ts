5fb44bad5adf9e0ef2f9cbcc0a1048ed98b8ca26
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-06-06 18:18:54 +0100

    add api client generated from openapi spec

---
+++ src/api-client/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CategoriesApi';
+export * from './MetadataApi';
+export * from './PostsApi';
+export * from './PreviewsApi';
+export * from './RelatedApi';
+export * from './SearchApi';
+export * from './TagsApi';
---
+++ src/api-client/models/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './MainCategoryDetail';
+export * from './MainCategoryInfo';
+export * from './MainCategoryTreeNode';
+export * from './MainErrorResponse';
+export * from './MainFrontMatter';
+export * from './MainMetadataResponse';
+export * from './MainMetadataResponseCategories';
+export * from './MainMetadataResponseConfig';
+export * from './MainMetadataResponsePosts';
+export * from './MainMetadataResponsePreviews';
+export * from './MainMetadataResponseTags';
+export * from './MainPost';
+export * from './MainPostPreview';
+export * from './MainPostsResponse';
+export * from './MainPreviewsResponse';
+export * from './MainRelatedPost';
