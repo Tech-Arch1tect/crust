8ba0586f66594b5a723bc3312c397907cd8c43ed
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-06-09 22:28:12 +0100

    British English spellings

--- src/lib/utils/category.ts
+++ src/lib/utils/category.ts
@@ -1,7 +1,7 @@
 export function getCategoryName(category: string | undefined | null) {
-	if (!category) return 'Uncategorized';
+	if (!category) return 'Uncategorised';
 	const categoryName = category.split('/').pop()?.replace('-', ' ');
 	return categoryName
 		? categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
-		: 'Uncategorized';
+		: 'Uncategorised';
 }
