3c653768d953a13d6106f97511914b608b722e50
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-06-12 17:49:22 +0100

    fix: 404 on error retrieving posts

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,12 +1,20 @@
 import { postsApi } from '$lib/api';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ parent, params }) => {
 	const { meta } = await parent();
 
-	const post = await postsApi.postsBySlugGet({
-		slug: params.slug
-	});
+	let post;
+
+	try {
+		post = await postsApi.postsBySlugGet({
+			slug: params.slug
+		});
+	} catch (apiError) {
+		console.error(apiError);
+		throw error(404, 'Post not found');
+	}
 
 	return {
 		meta,
